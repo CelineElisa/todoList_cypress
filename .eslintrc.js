224a08c4f5e7ac24d38e79ec959b065ca1a1e434
celinecottier <celine.cottier@saqara.com>
2024-02-18 20:50:54 +0100

    add ESlint config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'no-console': [
+            0
+        ]
+    }
+}
